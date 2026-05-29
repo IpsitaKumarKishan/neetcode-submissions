f72b682fb08ecf462d7510803db0595f4f476da2
Ipsita kumar kishan <ipsitakishan@gmail.com>
2026-05-29 21:56:49 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+        //count freq
+        for( int num : nums){
+            freq[num]++;
+        }
+        //bucket sort 
+        vector<vector<int>> bucket(nums.size()+1);
+
+        for(auto &p: freq){
+            bucket[p.second].push_back(p.first);
+        }
+
+        vector<int> ans;
+
+        for(int i=nums.size();i>=0 && ans.size() < k;i--){
+            for(int num : bucket[i]){
+                ans.push_back(num);
+                if(ans.size() == k)
+                return ans;
+            }
+        }
+        return ans;
+
+    }
+};
